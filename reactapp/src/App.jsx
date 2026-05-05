82cb46cd2f4d04080f7757be933f8eec8dc9d33e
Jiya Sinha <jiya15092007@gmail.com>
2026-05-05 15:09:01 +0530

    handle

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -83,24 +83,24 @@
 
 // FORM HANDLING :::::::::
 
-import { useState } from "react";
+// import { useState } from "react";
 
-function App() {
-  const [name, setName] = useState("");
+// function App() {
+//   const [name, setName] = useState("");
 
-  return (
-    <div>
-      <input
-        type="text"
-        placeholder="Enter name"
-        onChange={(e) => setName(e.target.value)}
-      />
-      <h2>{name}</h2>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <input
+//         type="text"
+//         placeholder="Enter name"
+//         onChange={(e) => setName(e.target.value)}
+//       />
+//       <h2>{name}</h2>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
 
 
 
