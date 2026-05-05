1addeee08f6cef94a16c5aa88dbe487e13c4276d
Jiya Sinha <jiya15092007@gmail.com>
2026-05-05 15:07:17 +0530

    counter

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -58,22 +58,22 @@
 
 // COUNTER APP:::::::
 
-// import { useState } from "react";
+import { useState } from "react";
 
-// function App() {
-//   const [count, setCount] = useState(0);
+function App() {
+  const [count, setCount] = useState(0);
 
-//   return (
-//     <div>
-//       <h1>{count}</h1>
-//       <button onClick={() => setCount(count + 1)}>Increment</button>
-//       <button onClick={() => setCount(count - 1)}>Decrement</button>
-//       <button onClick={() => setCount(0)}>Reset</button>
-//     </div>
-//   );
-// }
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+    </div>
+  );
+}
 
-// export default App;
+export default App;
 
 
 
